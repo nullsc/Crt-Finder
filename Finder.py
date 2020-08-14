f0a33682a45d627c8ea60cd4fc65991cf4ec038e
nullsc <53406164+nullsc@users.noreply.github.com>
2020-08-14 23:55:33 +0100

    Update Finder.py

--- Finder.py
+++ Finder.py
@@ -1,5 +1,25 @@
 #Work in progress
-import requests
+#Sub Domain finder using crt
 
-print("Type in the domain you want to search")
-domain = input()
+import requests, json, re
+from bs4 import BeautifulSoup
+print("Subdomain Finder")
+
+#https://crt.sh/?q=%.google.com&output=json
+domain = "starbucks.com"
+crtUrl = "https://crt.sh/?q=%." + domain + "" #
+print("Fetching:", crtUrl)
+
+domains = requests.get(crtUrl)
+test = BeautifulSoup(domains.text, "html.parser")
+#test.decode('utf-8')
+
+
+match = re.findall(r'<TD>(.*?)\.' + domain + '</TD>', domains.text) #works
+#print(match)
+for dom in match:
+    if "<BR>" in dom:
+        dom.replace("<BR>","\n")
+        #print("br one")
+    print(match)
+
